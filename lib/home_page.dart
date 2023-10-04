b326fcc6ed2f96675e3e325c34af66e858ca6885
enzolsmoura <enderenzo@gmail.com>
2023-10-04 13:13:50 -0300

    completo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool imagem = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,27 +25,49 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding: const EdgeInsets.all(50),
               child: Center(
-                child: DottedBorder(
-                  dashPattern: const [8, 8],
-                  color: Colors.black,
-                  child: Container(
-                    constraints: BoxConstraints.tight(const Size.fromRadius(150)),
-                    alignment: Alignment.center,
-                    color: const Color.fromARGB(100, 172, 172, 172),
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 70,
-                      child: const ContainerEl(),)
-                  ),
-                )
+                child: imagem == false
+                    ? DottedBorder(
+                        dashPattern: const [8, 8],
+                        color: Colors.black,
+                        child: GestureDetector(
+                            child: Container(
+                                constraints: BoxConstraints.tight(
+                                    const Size.fromRadius(150)),
+                                alignment: Alignment.center,
+                                color: const Color.fromARGB(100, 172, 172, 172),
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  height: 70,
+                                  child: const ContainerEl(),
+                                )),
+                            onTap: () {
+                              setState(() {
+                                imagem = true;
+                              });
+                            }))
+                    : Container(
+                        constraints:
+                            BoxConstraints.tight(const Size.fromRadius(150)),
+                        alignment: Alignment.center,
+                        child: const Image(
+                          image: NetworkImage(
+                              "https://yt3.googleusercontent.com/ytc/APkrFKab2owT4q0x6_6G1m5s9YqkHrRrpxfSXLPQbkxRZw=s176-c-k-c0x00ffffff-no-rj"),
+                          fit: BoxFit.contain,
+                        )),
               ),
             ),
             Center(
               child: ElevatedButton(
-                onPressed: () {  },
+                onPressed: () {
+                  setState(() {
+                    imagem = false;
+                  });
+                },
                 style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
                 ),
                 child: const Text("Resetar Imagem"),
               ),
